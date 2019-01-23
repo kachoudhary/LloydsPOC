f48eada63c85e78f46fddeb4b2ad03b8bd6ef72e
kachoudhary <kartikey.choudhary@bt.com>
2019-01-23 00:29:57 +0000

    apptheme changed to lighter2

--- app/src/main/java/com/bassem/lloydsPOC/utils/Constants.java
+++ app/src/main/java/com/bassem/lloydsPOC/utils/Constants.java
@@ -2,8 +2,8 @@ package com.bassem.lloydsPOC.utils;
 
 
 public class Constants {
-    public static final String API_KEY = "35066a49f2deb23a3c35fd48ff5c9869";
+    public static final String API_KEY = "018e7d31d8d00a27d73ff0a0f42e143e";
     public static String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
     public static final int TOP_ITEMS_LIMIT = 30;
-    public static final String DEFAULT_LASTFM_USER = "drrobbins";
+    public static final String DEFAULT_LASTFM_USER = "kachoudhary";
 }
